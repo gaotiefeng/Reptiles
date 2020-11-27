96a4066496cdd052c1ee1e9c54ad76a0dc086e3b
gaotiefeng <15904435047@163.com>
2020-11-27 17:59:32 +0800

    update process

---
+++ process.py
@@ -0,0 +1,33 @@
+# 多进程不共享全局变量
+import multiprocessing
+import time
+import os
+
+def write(q):
+        print("write启动(%s)，父进程为(%s)" % (os.getpid(), os.getppid()))
+        for i in "pythontest":
+                q.put(i)
+
+
+def read(q):
+        print("read启动(%s)，父进程为(%s)" % (os.getpid(), os.getppid()))
+        for i in range(q.qsize()):
+                print("read从Queue获取到消息：%s" % q.get(True))
+
+
+def main():
+    print("(%s) start" % os.getpid())
+    q = multiprocessing.Manager().Queue()
+    po = multiprocessing.Pool()
+    po.apply_async(write, args=(q,))
+
+    time.sleep(2)
+
+    po.apply_async(read, args=(q,))
+    po.close()
+    po.join()
+
+    print("(%s) end" % os.getpid())
+
+if __name__ == "__main__":
+    main()
---
+++ requestTest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+from RequestsClass import RequestsClass
+
+url = 'https://www.baidu.com'
+
+RequestsClass().get_content()
